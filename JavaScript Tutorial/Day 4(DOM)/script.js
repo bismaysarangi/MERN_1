cf9c569cc860d1fa6c66c41533b28b422738f83b
Bismay <bismaysarangi@gmail.com>
2024-06-08 23:28:20 +0530

    parent and siblings in js

--- JavaScript Tutorial/Day 4(DOM)/script.js
+++ JavaScript Tutorial/Day 4(DOM)/script.js
@@ -2,3 +2,21 @@
 
 let a = document.querySelectorAll('li');
 console.log(a);
+
+let b = document.querySelectorAll('top');
+console.log(b);
+
+//Parent Element
+let parent = document.querySelector('div');
+let p = parent.parentElement;
+console.log(p);
+
+//Previous Element Sibling
+var z = console.log(document.querySelector('.middle').previousElementSibling.previousElementSibling);
+
+if(z.previousElementSibling.previousElementSibling == null) {
+    console.log("Previous Previous Element sibling not present.");
+}
+else {
+    console.log(z.previousElementSibling.previousElementSibling);
+}
